93679e2514fa09192f38413b9e949fc52d2081e1
Michiel Appelman <michiel@appelman.se>
2017-08-18 13:11:29 +0200

    Changes to spaceship ZSH theme

--- zshrc.sh
+++ zshrc.sh
@@ -2,7 +2,9 @@
 source ~/.shrc
 
 # oh-my-zsh config
-export ZSH=$HOME/.oh-my-zsh
+ZSH=$HOME/.oh-my-zsh
+ZSH_CUSTOM=$HOME/.zsh_custom
+ZSH_THEME=spaceship
 HIST_STAMPS="dd.mm.yyyy"
 DISABLE_UPDATE_PROMPT=true
 
@@ -31,36 +33,6 @@ bindkey '^A' beginning-of-line
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 bindkey '\e.' insert-last-word
-
-# Prompt Styling
-
-function user {
-    [[ $(id -u) -eq 0 ]] && echo "%{$fg[red]%}%n%{$reset_color%}" && return
-    echo "%{$fg[green]%}%n%{$reset_color%}"
-}
-function sh_char {
-    [[ $(id -u) -eq 0 ]] && echo "%{$fg[red]%}#%{$reset_color%}" && return
-    echo "%{$fg[cyan]%}$%{$reset_color%}"
-}
-function error_flag {
-    [[ $? -ne 0 ]] && echo "%{%F{red}%}\u2718 "
-}
-function pyenv_str {
-    if $(pyenv local >/dev/null 2>&1) ; then
-        echo "@ %F{magenta}$(pyenv version | cut -f 1 -d ' ')%f "
-    fi
-}
-
-MODE_INDICATOR="$FX[bold]$FG[020]<$FX[no_bold]%{$fg[blue]%}<<%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_PREFIX="on %{$fg[yellow]%}\uE0A0 "
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%} ✗"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[yellow]%} ?"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green] ✔"
-
-PROMPT='$(error_flag)$(user) %B»%b %3~ $(git_prompt_info)%{$reset_color%} $(pyenv_str)
-$(sh_char) '
-RPS1='%{$fg[magenta]%}%*%{$reset_color%}'
 
 # Run archey if available:
 if $(which archey >/dev/null 2>&1); then
