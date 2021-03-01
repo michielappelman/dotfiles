7d4afbd254b5a8b7896749c5ee424c9b60c84833
Michiel Appelman <michiel@appelman.se>
2021-03-01 13:39:21 +0100

    Adds `z` directory jumping to zsh, and updates some other zsh settings

--- zshrc.sh
+++ zshrc.sh
@@ -1,24 +1,14 @@
-zmodload zsh/zprof
+# zmodload zsh/zprof
 
 # check if this is a login shell
 [ "$0" = "-zsh" ] && export LOGIN_ZSH=1
-
 # run zprofile if this is not a login shell
 [ -n "$LOGIN_ZSH" ] && source ~/.zprofile
 
-# load shared shell configuration
 source ~/.shrc
-
-# History file
 export HISTFILE=~/.zsh_history
-
-# Don't show duplicate history entires
 setopt hist_find_no_dups
-
-# Remove unnecessary blanks from history
 setopt hist_reduce_blanks
-
-# Share history between instances
 setopt share_history
 
 # oh-my-zsh config
@@ -46,13 +36,18 @@ SPACESHIP_PROMPT_ORDER=(
   exit_code     # Exit code section
   char          # Prompt character
   )
-SPACESHIP_VI_MODE_INSERT="${SPACESHIP_VI_MODE_INSERT:="i"}"
-SPACESHIP_VI_MODE_NORMAL="${SPACESHIP_VI_MODE_NORMAL:="n"}"
+SPACESHIP_VI_MODE_INSERT="I"
+SPACESHIP_VI_MODE_NORMAL="N"
+SPACESHIP_VI_MODE_COLOR="yellow"
 SPACESHIP_GIT_SYMBOL="🎋  "
 SPACESHIP_VENV_SHOW=false
 
-plugins=(zsh-autosuggestions sublime brew git mosh rsync python tmux osx history httpie vi-mode)
+plugins=(sudo zsh-z zsh-autosuggestions sublime brew git mosh rsync python tmux osx history httpie vi-mode)
 source $ZSH/oh-my-zsh.sh
+
+eval spaceship_vi_mode_enable
+
+zstyle ':completion:*' menu select
 
 ##### Key bindings #####
 bindkey '^S' sudo-command-line
